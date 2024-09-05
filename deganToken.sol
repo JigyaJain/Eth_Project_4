5b2bdde4b4876d35ccde16f74f8ab3e773bcadf2
JigyaJain <164784960+JigyaJain@users.noreply.github.com>
2024-09-05 15:15:29 +0530

    deganToken.sol

--- deganToken.sol
+++ deganToken.sol
@@ -5,15 +5,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract degenToken is ERC20{
     address private owner;
-    mapping (address => uint) public bal;
+    mapping (address => uint) private bal;
     
     struct Reward {
         string name;
         uint cost; // Cost in tokens
     }
 
-    Reward[] public rewards;   
+    Reward[] private rewards;   
         // Predefined rewards  
+    
+    struct Inventory {
+        string name;
+        uint cost; // Cost in tokens
+        address account;
+    }
+
+    Inventory[] private inventory;   
+        // Predefined rewards 
 
     constructor() ERC20( "Degen", "DGN"){
         owner = msg.sender;
@@ -22,47 +31,54 @@ contract degenToken is ERC20{
         rewards.push(Reward("Health Potion", 2500));           
     }
 
-    function mintToken( address _to, uint _amount) public returns (address) {  
+    function aaA_mintToken( address _to, uint _amount) public returns (address) {  
         require ( owner == msg.sender, "Only contract owner can mint tokens");
         _mint(_to, _amount);
         bal[_to] = _amount;
         return _to;
     }
 
-    function transferToken(address _from, address _to, uint _amount) public returns (uint, uint)  {
+    function aaB_transferToken( address _to, uint _amount) public returns (uint, uint)  {
         
-       require( bal[_from]>= _amount, "Low Balance");
-        _transfer(_from, _to, _amount);
+       require( bal[msg.sender]>= _amount, "Low Balance");
+        _transfer(msg.sender, _to, _amount);
 
-        bal[_from] -= _amount;
+        bal[msg.sender] -= _amount;
         bal[_to] += _amount;
 
-        return (bal[_from], bal[_to]);
+        return (bal[msg.sender], bal[_to]);
 
     }
 
     // Function to redeem reward
-    function redeem(address _add, uint _rewardId) external returns (uint, string memory){
+    function aaC_redeem(uint _rewardId) external returns (uint, string memory){
         require(_rewardId < rewards.length, "Invalid reward ID");
         Reward memory reward = rewards[_rewardId];
-        require(bal[_add] >= reward.cost, "Insufficient balance to redeem this reward");
+        require(bal[msg.sender] >= reward.cost, "Insufficient balance to redeem this reward");
 
         // Burn the tokens
-        _burn(_add, reward.cost);
-        bal[_add] -= reward.cost;
+        _burn(msg.sender, reward.cost);
+        bal[msg.sender] -= reward.cost;
 
-        return (bal[_add], "redeemed");
+        address acc = msg.sender;
+
+        inventory.push(Inventory(reward.name,reward.cost,acc));
+
+        return (bal[msg.sender], "redeemed");
     }
 
-    function burnToken( address _add, uint _amount) external {
-        require (bal[_add] >= _amount, "You should burn some less token");
-        _burn(_add, _amount);
+    function aaD_burnToken(uint _amount) external {
+        require (bal[msg.sender] >= _amount, "You should burn some less token");
+        _burn(msg.sender, _amount);
     }
 
+    function aaE_getInventory()external view returns(Inventory[] memory)
+    {
+        return inventory;
+    }
+
+    function aaE_getRewards()external view returns(Reward[] memory)
+    {
+        return rewards;
+    }
 }
-
-
-/*
-0xFae9148E6458787B5737686F98A1E3161f8dEcb2
-0xD57Cc0AD0c3ff4D5a13bf3fDe9Ab9624f4A282db
-*/
